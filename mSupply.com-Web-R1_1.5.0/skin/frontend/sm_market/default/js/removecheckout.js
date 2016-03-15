87e6b21d20cf33011747687999dba505b0489342
badarils <lsbadari@gmail.com>
2016-03-15 11:58:07 +0530

    deleted

--- mSupply.com-Web-R1_1.5.0/skin/frontend/sm_market/default/js/removecheckout.js
+++
@@ -1,7 +0,0 @@
-var NilaaOpcheckout = Class.create(Checkout, {
-    initialize: function($super,accordion, urls){
-        $super(accordion, urls);
-        //New Code Addded
-        this.steps = ['login','billing', 'shipping', 'review', 'payment'];
-    }
-});
